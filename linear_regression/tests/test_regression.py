a054ca28c3a046f8d5e2bff1cc2d183861ce492f
Pierre Ablin <pierreablin@gmail.com>
2021-11-19 17:20:48 +0100

    fix workflow

--- linear_regression/tests/test_regression.py
+++ linear_regression/tests/test_regression.py
@@ -4,7 +4,7 @@ from linear_regression import linear_regression
 
 def test_dimensions():
     X = np.random.randn(3, 2)
-    y = np.random.randn(3 )
+    y = np.random.randn(3)
     coefs = linear_regression(X, y)
     assert len(coefs) == 2
 
