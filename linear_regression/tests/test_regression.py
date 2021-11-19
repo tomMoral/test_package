d64a84be54d10370b6e39519842bbbc706313d74
Pierre Ablin <pierreablin@gmail.com>
2021-11-19 17:15:39 +0100

    added tests

---
+++ linear_regression/tests/test_regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+from linear_regression import linear_regression
+
+
+def test_dimensions():
+    X = np.random.randn(3, 2)
+    y = np.random.randn(3)
+    coefs = linear_regression(X, y)
+    assert len(coefs) == 2
+
