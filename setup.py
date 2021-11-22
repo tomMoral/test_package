d9a7692e264b26b722bc25a0a69ff74889d72e29
Pierre Ablin <pierreablin@gmail.com>
2021-11-22 17:51:29 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,53 +1,9 @@
 #! /usr/bin/env python
 
-import os
-from setuptools import setup, find_packages
-
-descr = """Linear regression"""
-
-version = None
-with open(os.path.join('linear_regression', '__init__.py'), 'r') as fid:
-    for line in (line.strip() for line in fid):
-        if line.startswith('__version__'):
-            version = line.split('=')[1].strip().strip('\'')
-            break
-if version is None:
-    raise RuntimeError('Could not determine version')
+from setuptools import setup
 
 
-DISTNAME = 'linear_regression'
-DESCRIPTION = descr
-MAINTAINER = 'Pierre Ablin'
-MAINTAINER_EMAIL = 'pierreablin@gmail.com'
-LICENSE = 'MIT'
-DOWNLOAD_URL = None
-VERSION = version
-URL = None
-
-if __name__ == "__main__":
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          version=VERSION,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          install_requires=[
-              'numpy >=1.16.0',
-          ],
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-          ],
-          platforms='any',
-          packages=find_packages(),
-          )
+setup(name='linear_regression',
+      install_requires=['numpy',],
+      packages=['linear_regression']
+      )
