832463695e639e15961aeae902d285a3cb1e54ba
Thomas Moreau <thomas.moreau.2010@gmail.com>
2023-02-15 15:59:52 +0100

    CLN simplify packaging and add some options in GA (#1)

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #! /usr/bin/env python
-
 from setuptools import setup
 
 
-setup(name='linear_regression',
-      install_requires=['numpy',],
-      packages=['linear_regression']
-      )
+setup()
