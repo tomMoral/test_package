ca38f0c354cf969e9255edbbf9f50e6875e2e4c4
alexis <alexisthual@gmail.com>
2023-02-23 12:17:09 +0100

    MTN only use pyproject.toml for packaging (#2)
    Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Doc, install and writing code
 - Clone the repository on your computer
 - Write a doc for the function
 - Organize code as a package : `folder/code/functions.py`, and add an `__init__.py` file, in which only the version is present.
-- At the root, write a setup.py function that will allow to install the package, and then do `pip install -e.`. This should not return any error.
+- At the root, write a pyproject.toml function that will allow to install the package, and then do `pip install -e.`. This should not return any error.
 - In the code folder, create a `tests` folder, a `test_linear_regression.py` file in it and write several test functions, with names starting with `test_`. Then, run `pytest` at the root file. All tests should pass
 - Check that the code is compatible with pep 8 using the command `flake8`
 - Upload everything on github by first adding all new files using `git add ...`, then commiting by using `git commit -am ` (write a message here) and then `git push`. Check on github that the new code is here.
--- linear_regression/tests/test_regression.py
+++ linear_regression/tests/test_regression.py
@@ -6,30 +6,35 @@ from linear_regression import least_squares
 
 @pytest.mark.parametrize("n", [2, 10, 20])
 def test_dimensions(n):
-    X = np.random.randn(n, 3)
-    y = np.random.randn(n)
+    rng = np.random.RandomState(0)
+    X = rng.randn(n, 3)
+    y = rng.randn(n)
     coefs = least_squares(X, y)
     assert len(coefs) == 3
 
 
 def test_minimum():
-    X = np.random.randn(3, 2)
-    y = np.random.randn(3)
+
+    rng = np.random.RandomState(0)
+    X = rng.randn(3, 2)
+    y = rng.randn(3)
     coefs = least_squares(X, y)
     gradient = np.dot(X.T, np.dot(X, coefs) - y)
     assert_almost_equal(gradient, 0)
 
 
 def test_no_nan():
-    X = np.random.randn(3, 2)
-    y = np.random.randn(3)
+    rng = np.random.RandomState(42)
+    X = rng.randn(3, 2)
+    y = rng.randn(3)
     coefs = least_squares(X, y)
     assert not np.isnan(coefs).any()
 
 
 def test_recovery():
-    X = np.random.randn(3, 2)
-    true_coefs = np.random.randn(2)
+    rng = np.random.RandomState(0)
+    X = rng.randn(3, 2)
+    true_coefs = rng.randn(2)
     y = np.dot(X, true_coefs)
     predicted_coefs = least_squares(X, y)
     assert_almost_equal(true_coefs, predicted_coefs)
